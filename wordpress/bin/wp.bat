cd7fa97245a0a81f9f21ccf3c872f67326aa0f81
MathewLister <listermathew0@gmail.com>
2021-01-05 16:03:05 -0800

    wordpress

---
+++ wordpress/bin/wp.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"C:\Users\angry\Desktop\wordpress/php/php" "C:\Users\angry\Desktop\wordpress/apps/wordpress/bin/wp-cli.phar" --path="C:\Users\angry\Desktop\wordpress/apps/wordpress/htdocs" %*
